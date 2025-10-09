e2ca2d3f6edbbd40142dc00dde325205cc4c05e5
Lilly Schwarzenbach <lillyschwarzenbach@gmail.com>
2025-10-09 12:26:17 +0200

    add runners using all fish in a single experiment

---
+++ run_lei2020_exp151_fishall_orientation_cosine.py
@@ -0,0 +1,19 @@
+from configs.config_exp151_fish3_orientation_nowall_cosine import ConfigSpecific
+from enums.sorting_criteria_enum import SortingCriteria
+from enums.loss_functions_enum import LossFunctions
+from enums.wall_behaviour_enum import WallBehaviourType
+from experiments.experiment_runner import ExperimentRunner
+
+config = ConfigSpecific()
+config.sorting_criteria = SortingCriteria.ORIENTATION
+config.loss_function = LossFunctions.COSINE
+config.fish_ids = []
+
+for wall_behaviour_type in [WallBehaviourType.NO_WALL,
+                            WallBehaviourType.REPULSION_ONLY,
+                            WallBehaviourType.ALIGN_ONLY,
+                            WallBehaviourType.REPULSION_AND_ALIGNMENT]:
+    config.wall_behaviour = wall_behaviour_type
+    config.name = None
+    runner = ExperimentRunner(config, save_location="results/")
+    runner.run_experiments()
--- run_orientation_nowall_cosine.py
+++
@@ -1,5 +0,0 @@
-from configs.config_orientation_nowall_cosine import ConfigOrientationNoWallCosine
-from experiments.experiment_runner import ExperimentRunner
-
-runner = ExperimentRunner(ConfigOrientationNoWallCosine(), save_location="results/")
-runner.run_experiments()
